b28cdf29a09f203c14743f0501ac2936b871c591
flyforday <leo.lemaire@outlook.com>
2020-04-21 14:50:00 +0200

    connexion prive

--- src/connexion.php
+++ src/connexion.php
@@ -1,6 +1,4 @@
 <?php
-        $db = "mysql:host=91.216.107.162;dbname=leole1207283_3pjmjf";
-        $user = "leole1207283_3pjmjf";
-        $password = "mfy8rauhvo";
+        require "secret.php";
         $dbh = new PDO($db,$user,$password);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
